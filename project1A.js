ececaeb78587b25654896ce65a870eaf7320a3bd
meeniebeanie <minyen.j@gmail.com>
2016-07-11 03:25:04 +0800

    everything works fine now, next- to add more questions to the question bank. to add randomize function to randomly populate questions.

--- project1A.js
+++ project1A.js
@@ -1,26 +1,45 @@
-
 console.log("loaded");
 
-//get the name of your players!
+$(document).ready(function(){
+  console.log("ready!");
+});
 
-var getName1= prompt("Hey what's your name?");
-var getName2= prompt("And your buddy's name?");
+//get the name of your players!
+var getName1 = "";
+var getName2 = "";
+
+function nullNameCheck() {
+  if (getName1 === ""){
+    getName1 = "Player 1";
+    return getName1;
+  }
+  else if (getName2 === ""){
+    getName2 = "Player 2";
+    return getName2;
+  }
+}
+
+getName1 = prompt("Hey what's your name?");
+nullNameCheck();
+getName2 = prompt("And your buddy's name?");
+nullNameCheck();
 
 var player1 = $("#player1").html(getName1);
 var player2 = $("#player2").html(getName2);
+
 
 //switch turn
 
 var playerTurn = 2;
 function changeTurn(){
-  if (playerTurn == 2){
-    playerTurn = 1;
-    $(".turn").html("It's " + getName1+"'s turn!");
-    // console.log("it's " + getName1+"'s turn!");
-  } else {
+  if (playerTurn == 1){
     playerTurn = 2;
     $(".turn").html("It's " + getName2+"'s turn!");
-    // console.log("it's " + getName2+"'s turn!");
+    console.log("it's " + getName2+"'s turn!");
+  } else {
+    playerTurn = 1;
+    $(".turn").html("It's " + getName1+"'s turn!");
+    console.log("it's " + getName1+"'s turn!");
   }
 }
 changeTurn();
@@ -54,19 +73,22 @@ function Set(question, answer) {
 }
 
 //this are all your q&a sets!
-var set1 = new Set('This is question1', true);
-var set2 = new Set('This is question2', false);
-var set3 = new Set('This is question3', true);
-var set4 = new Set('This is question4', false);
-var set5 = new Set('This is question5', true);
-var set6 = new Set('This is question6', false);
-var set7 = new Set('This is question7', true);
-var set8 = new Set('This is question8', false);
-var set9 = new Set('This is question9', true);
-var set10 = new Set('This is question10', false);
+var set1 = new Set('Cows do have best friends and become stressed when they are separated.', true);
+var set2 = new Set('Vikings had horns on their helmets.', false);
+var set3 = new Set('The term "junkie" comes from the fact that recreational heroin users in the early 1900s would sell scrap metal to pay for their heroin.', true);
+// var set4 = new Set('Tomatoes are vegetables.', false);
+var set4 = new Set('I\'m fine.', false);
+var set5 = new Set('Turtles can breathe through their butts and pee through their mouths.', true);
+var set6 = new Set('Dogs sweat through their tongue.', false);
+var set7 = new Set('Fingers prune underwater because of an evolutionary trait caused by the brain to enhance the grip of your fingers underwater.', true);
+// var set8 = new Set('Bats are blind.', false);
+var set8 = new Set('It\'s not you, its me.', false);
+var set9 = new Set('Eskimos use refrigerators to keep their food from freezing', true);
+var set10 = new Set('Goldfishes have only 3 second memory.', false);
+
+
 
 //var random question randomizes questions, pairs the random question to random answer.
-// var random = Math.floor((Math.random() * 10));
 
 var random = 0;
 var randomAnswer = false;
@@ -75,59 +97,70 @@ var quest = $(".questionsText");
 
 //next question function
 function nextQuestion() {
-  random = Math.floor(Math.random() * setArray.length);
-  randomQuestion = setArray[random].question;
-  randomAnswer = setArray[random].answer;
-  console.log(setArray + "before");
-  setArray.splice(random, 1);
-  console.log(setArray + "after");
+  if (setArray.length > 0) {
+    random = Math.floor(Math.random() * setArray.length);
+    randomQuestion = setArray[random].question;
+    randomAnswer = setArray[random].answer;
+    setArray.splice(random, 1);
+    console.log(setArray.length + " questions left.");
 
-  //change class=questionText in html to the question text inside the setArray array
-  quest.text(randomQuestion);
+    //change class=questionText in html to the question text inside the setArray array
+    quest.text(randomQuestion);
+  }
+  else {}
 }
 nextQuestion();
 
 //reset function
-function reset() {
+
+function reset(){
   setArray = [set1, set2, set3, set4, set5, set6, set7, set8, set9, set10];
   player1Score = 0;
   $("#score1").html(player1Score);
   player2Score = 0;
   $("#score2").html(player2Score);
+  quest.removeClass("p1WinText");
+  quest.removeClass("p2WinText");
+  quest.removeClass("tieText");
+  nextQuestion();
 }
 
 //check win
+
 function checkWin() {
   if (setArray.length === 0) {
     if (player1Score > player2Score){
-      alert(getName1 + " wins!");
-      reset();
+      quest.text(getName1 + " wins!").addClass("p1WinText");
+      console.log(getName1 + " wins!");
+      setTimeout(reset, 2000);
+    }
+    else if (player1Score < player2Score){
+      quest.text(getName2 + " wins!").addClass("p2WinText");
+      console.log(getName2 + " wins!");
+      setTimeout(reset, 2000);
     }
     else {
-      alert(getName2 + " wins!");
-      reset();
+      quest.text("It\'s a tie!").addClass("tieText");
+      console.log("It's a tie!");
+      setTimeout(reset, 2000);
     }
-  } else {}
+  }
 }
-
-
+checkWin();
 
 //what happens if player clicks true
 var trueButton = $(".true");
 trueButton.click(function() {
-  var ansTrue = true;
-  if (ansTrue == randomAnswer) {
+  if (true === randomAnswer) {
     quest.html("<p class='right'>You got it right!</p>");
-    console.log("You got it right! click enter for the next question.");
     addScore();
     console.log(getName1 + "'s score is: " + player1Score);
     console.log(getName2 + "'s score is: " + player2Score);
   }
   else {
     quest.html("<p class='wrong'>Nope the answer is False</p>");
-    console.log("Nope the answer is False! click enter for the next question.");
   }
-  setTimeout(nextQuestion, 1500);
+  setTimeout(nextQuestion, 250);
   checkWin();
   changeTurn();
 });
@@ -135,19 +168,16 @@ trueButton.click(function() {
 //what happens if player clicks false
 var falseButton = $(".false");
 falseButton.click(function() {
-  var ansFalse = false;
-  if (ansFalse == randomAnswer) {
+  if (false === randomAnswer) {
     quest.html("<p class='right'>You got it right!</p>");
-    console.log("You got it right! click enter for the next question.");
     addScore();
     console.log(getName1 + "'s score is: " + player1Score);
     console.log(getName2 + "'s score is: " + player2Score);
   }
   else {
     quest.html("<p class='wrong'>Nope the answer is False</p>");
-    console.log("Nope the answer is True! click enter for the next question.");
   }
-  setTimeout(nextQuestion, 1500);
+  setTimeout(nextQuestion, 250);
   checkWin();
   changeTurn();
 });
